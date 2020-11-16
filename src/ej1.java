730128ffc42426619dc63e02c20eed68fc72b3bd
MBVTracks <miguel037.mb@gmail.com>
2020-11-16 19:44:44 +0100

    HolaMCambio

--- src/ej1.java
+++ src/ej1.java
@@ -1,5 +1,6 @@
 public class ej1 {
     public static void main(String[] args) {
         System.out.println("Cambio desde el proyecto de CarpetaCasa");
+        System.out.println("Hola mundo, que haces?");
     }
 }
