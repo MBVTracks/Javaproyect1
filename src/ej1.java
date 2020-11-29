ab6eb8da0658f4ab45a21c9a4e3991495867740a
MBVTracks <miguel037.mb@gmail.com>
2020-11-29 19:40:51 +0100

    Cambio ramaMaster

--- src/ej1.java
+++ src/ej1.java
@@ -1,9 +1,6 @@
 public class ej1 {
     public static void main(String[] args) {
-        System.out.println("Cambio desde el proyecto de CarpetaCasa");
-        System.out.println("Hola mundo, ¿que haces?");
-        System.out.println("Nada, Observa a la gente.");
-        System.out.println("Tags");
+        System.out.println("Cambio desde el proyecto de CarpetaCasa, rama master");
 
 
     }
